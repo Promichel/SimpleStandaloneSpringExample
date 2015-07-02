091151f6fb7134c456b842320b802ebd4f135b22
Patrick Trautmann <patrick.trautmann@gmail.com>
2015-07-02 22:31:03 +0200

    Example with Property File values

--- src/main/java/de/patravis/simplespring/config/ApplicationConfiguration.java
+++ src/main/java/de/patravis/simplespring/config/ApplicationConfiguration.java
@@ -3,12 +3,24 @@ package de.patravis.simplespring.config;
 import de.patravis.simplespring.Starter;
 import de.patravis.simplespring.beans.User;
 import de.patravis.simplespring.beans.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@PropertySource("classpath:test.properties")
 public class ApplicationConfiguration {
+
+    @Value("${king.name}")
+    private String kingsName;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public Starter starter() {
@@ -24,7 +36,7 @@ public class ApplicationConfiguration {
     @Scope("prototype")
     public User user() {
         User user = new User();
-        user.setName("Patrick");
+        user.setName(kingsName);
 
         return user;
     }
